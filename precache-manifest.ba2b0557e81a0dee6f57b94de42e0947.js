43f13371d0ad292f6d55b4a44ea073571abaa022
Maiki <rdemaiki@gmail.com>
2020-07-19 14:55:06 -0300

    Updates

---
+++ precache-manifest.ba2b0557e81a0dee6f57b94de42e0947.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cbc9083345288719378afeb6c2dbb6",
+    "url": "/amigobo/index.html"
+  },
+  {
+    "revision": "16d847c37c3a2fe7d8ce",
+    "url": "/amigobo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e8fa10e79f479392430d",
+    "url": "/amigobo/static/css/main.45aeab73.chunk.css"
+  },
+  {
+    "revision": "16d847c37c3a2fe7d8ce",
+    "url": "/amigobo/static/js/2.0e2e6646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amigobo/static/js/2.0e2e6646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fa10e79f479392430d",
+    "url": "/amigobo/static/js/main.c95067a3.chunk.js"
+  },
+  {
+    "revision": "8aeb9db25031691dfc0e",
+    "url": "/amigobo/static/js/runtime-main.16b64c54.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/amigobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
